abc5b69d2bbb762edbf34f214c90bae1a98a96e7
Franck <franck.schmitt@hotmail.com>
2018-12-08 09:11:44 +0100

    day five part_one

---
+++ day5/part_one/solution.py
@@ -0,0 +1,24 @@
+# Should return 9172
+def get_polymer():
+    with open('input') as f:
+        return f.readline()
+
+
+# @todo: need improvement
+def get_reacted_polymer(chain):
+    change_happens = True
+    while change_happens:
+        change_happens = False
+        for pos, polymer in enumerate(chain):
+            if pos < len(chain) - 1 and chain[pos+1] == polymer.swapcase():
+                change_happens = True
+                chain = ''.join([chain[:pos], chain[pos+2:]])
+                break
+    return chain
+
+
+line = get_polymer()
+final_polymer = get_reacted_polymer(line)
+
+print(final_polymer)
+print(len(final_polymer))
